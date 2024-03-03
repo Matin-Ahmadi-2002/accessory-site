0245012d27fb8b089829f068a1d6ecdb9b037a69
Matin-Ahmadi-2002 <miranahmadi2002@gmail.com>
2024-03-03 01:14:44 -0800

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    fontFamily: {
+      "iransans" : "iransans",
+      "iransansBold" : "iransansBold",
+      "iransansLight" : "iransansLight",
+      "iransansMedium" : "iransansMedium",
+      "iransansUltralight" : "iransansUltralight",
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
