46a503e0ca04d9a78426e9d3ac43f4da000ff020
Matin-Ahmadi-2002 <miranahmadi2002@gmail.com>
2024-03-06 00:13:56 -0800

    responsive woman collection slider section

--- js/main.js
+++ js/main.js
@@ -19,18 +19,22 @@ backButton.addEventListener('click', () => {
 $('#woman_slider').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
+    nav:false,
     dots:false,
     autoplay:false,
     responsive:{
         0:{
             items:3
         },
-        768:{
+        450:{
             items:4
         },
-        1000:{
-            items:5
+        768:{
+            items:5,
+        },
+        992:{
+            items:5,
+            nav:true,
         }
     }
 })
